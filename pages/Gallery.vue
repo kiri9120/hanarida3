e30da10c45a34d24324311bfc2256a0f4f73ea11
Masato Kiryu <kiri9120@gmail.com>
2019-11-19 00:24:09 +0900

    add bg-loop

--- pages/Gallery.vue
+++ pages/Gallery.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="member">
+    <div class="bg-image-loop"></div>
     <v-container>
       <h1 class="headline-type1 my-12">Gallery</h1>
-      <v-parallax src="../assets/flower-bg.jpg" height="400">
-      </v-parallax>
     </v-container>
   </div>
 </template>
 
 <style scoped lang="scss">
+.bg-image-loop {
+	width: 100%;
+	height: 600px; /* 画像の高さを指定 */
+	position: relative;
+	background: url("../static/img/slide-sample.jpg") repeat-x 0 0;
+	background-size: auto 100%;
+	animation: bgSlider 200s linear infinite;
+	padding: 0;
+
+  &:hover {
+    animation-play-state: paused;
+  }
+}
+@keyframes bgSlider {
+	from { background-position: 0 0; }
+    to { background-position: -13172px 0; }
+}
 </style>
